7efec345f002f6f462c4a9e3d927d4deabad8644
Andreas Holzner <andreasholzner@users.noreply.github.com>
2014-11-16 22:50:26 +0100

    GH-4 disable readline-complete as it is not available as package

--- config/common/.emacs.d/config/60-shell.el
+++ config/common/.emacs.d/config/60-shell.el
@@ -5,7 +5,7 @@
 (setq explicit-shell-file-name "bash")
 (setq explicit-bash-args '("-c" "export EMACS=; stty echo; bash"))
 (setq comint-process-echoes t)
-(require 'readline-complete)
+;; (require 'readline-complete)
 ;; (add-to-list 'ac-modes 'shell-mode)
 ;; (add-hook 'shell-mode-hook 'ac-rlc-setup-sources)
 
