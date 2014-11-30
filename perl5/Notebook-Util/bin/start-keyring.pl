2f076ef750a007c18b36e0158562fe9fc4663df6
Andreas Holzner <andreasholzner@users.noreply.github.com>
2014-11-30 01:37:04 +0100

    GH-5, GH-6 add logging to start-keyring script

--- perl5/Notebook-Util/bin/start-keyring.pl
+++ perl5/Notebook-Util/bin/start-keyring.pl
@@ -6,16 +6,22 @@ use warnings;
 use lib qw(/usr/lib/fvwm/2.6.5);
 use FvwmCommand;
 use List::Util qw(first);
+use File::Spec::Functions;
+use Log::Log4perl qw(:easy);
+
+Log::Log4perl->easy_init({ level => $INFO, file => '>>' . catfile($ENV{HOME}, '.xsession.log') });
 
 my @export_keys = qw(GNOME_KEYRING_CONTROL SSH_AUTH_SOCK GPG_AGENT_INFO GNOME_KEYRING_PID);
 
 my @output = `/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg`;
+DEBUG @output;
 
 foreach my $key (@export_keys) {
     my $line = first { /$key/ } @output;
     if ($line) {
-    my $value = (split /=/, $line)[1];
+        my $value = (split /=/, $line)[1];
         chomp $value;
+        DEBUG "setting environment: $key => $value";
         FvwmCommand::FvwmCommand('SetEnv', $key, $value);
     }
 }
