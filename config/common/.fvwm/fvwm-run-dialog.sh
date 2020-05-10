6ae1d556aa6d1f285eab2c15a85d8cb6d65a2512
Andreas Holzner <andreas.holzner@tngtech.com>
2020-05-10 13:13:20 +0200

    GH-19 Remove Fvwm config

--- config/common/.fvwm/fvwm-run-dialog.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-urxvt -pe "-digital-clock,-searchable-scrollback,-selection-popup,-option-popup" +sb -name "Ausführen" -title "Ausführen" -geometry 80x1+500+10 --keysym.0xFF0D " & exit\n" -e bash --init-file $HOME/.fvwm/run-bash-init
